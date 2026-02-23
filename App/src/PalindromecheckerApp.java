e5dd0dab623006a2042d60eae29fe20f45620f5a
harshita070913 <harshithabts@gmail.com>
2026-02-23 10:11:54 +0530

    RISHI add UC1

---
+++ App/src/PalindromecheckerApp.java
@@ -0,0 +1,14 @@
+public class PalindromecheckerApp{
+    public static void main(string[] args){
+system.out.println("welcome to palindrome checker app management system");
+system.out.println("Version: 1.0");
+}
+}
+
+
+
+
+
+
+
+
